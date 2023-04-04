ad3746c66b69c75fc014fee68c6ca3777628a280
sowjanyavaka1987 <113628962+sowjanyavaka1987@users.noreply.github.com>
2023-04-04 20:36:35 +0530

    changed in the develop branch

--- src/org/epam/SampleClass.java
+++ src/org/epam/SampleClass.java
@@ -4,7 +4,7 @@ public class SampleClass {
 
 	public static void main(String[] args) {
 		System.out.print("This is a sample app");
-		System.out.print("This is a sample app1");
+		System.out.print("This is a sample app1 in develop branch");
 
 	}
 
