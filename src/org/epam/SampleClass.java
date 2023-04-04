62cb501f3334ec625e702901f8f3c7c494b1bb88
sowjanyavaka1987 <113628962+sowjanyavaka1987@users.noreply.github.com>
2023-04-04 20:52:47 +0530

    test for stash

--- src/org/epam/SampleClass.java
+++ src/org/epam/SampleClass.java
@@ -5,7 +5,7 @@ public class SampleClass {
 	public static void main(String[] args) {
 		System.out.print("This is a sample app");
 		System.out.print("This is a sample app1 in develop branch");
-
+		System.out.print("This is a sample app1 in develop branch to stash");
 	}
 
 }
